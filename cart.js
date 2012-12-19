3671e358d9ac9af9fc5e76d46fe85101c38412a8
Lisa Rydje <lisa.rydje@gmail.com>
2012-12-19 23:41:41 +0100

    Removed old unused stuff

--- cart.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('#cart').after($('<button id="reset-cart">Nollställ cart</button>'));
-
-	$('#reset-cart').click(function() {
-		cart = [];
-		store.set('cart', cart);
-		$('#cart').empty();
-	});
-});
